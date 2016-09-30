723b4514198993352746447a5353081b35bedddd
Marco <marco.turi@hotmail.it>
2016-09-30 12:25:37 +0200

    fix(e2e): fix protractor port

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -30,7 +30,7 @@ exports.config = {
         isVerbose: true,
         includeStackTrace: false
     },
-    baseUrl: 'http://localhost:8102',
+    baseUrl: 'http://localhost:8100',
     allScriptsTimeout: 30000,
     // hook into screenshotReporter's beforeLaunch
     beforeLaunch: function() {
