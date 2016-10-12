d7a637f48fd7f6b735bcdfc2441880d19126d448
Marco Turi <marco.turi@hotmail.it>
2016-10-12 18:09:23 +0200

    feat(hooks): add hook to update version from package.json

---
+++ hooks/before_prepare/010_update_config.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+// Save hook under `project-root/hooks/before_prepare/`
+// https://gist.github.com/ohh2ahh/f35ff6e0d9f8b4268cdb
+// Don't forget to install xml2js using npm
+// `$ npm install xml2js`
+
+var fs = require('fs');
+var xml2js = require('xml2js');
+var packageJson = require('../../package.json');
+
+// Read config.xml
+fs.readFile('config.xml', 'utf8', function(err, data) {
+    if(err) {
+        return console.log(err);
+    }
+
+    // Get XML
+    var xml = data;
+
+    // Parse XML to JS Obj
+    xml2js.parseString(xml, function (err, result) {
+        if(err) {
+            return console.log(err);
+        }
+
+        // Get JS Obj
+        var obj = result;
+
+        // // ios-CFBundleVersion doen't exist in config.xml
+        // if(typeof obj['widget']['$']['ios-CFBundleVersion'] === 'undefined') {
+        //     obj['widget']['$']['ios-CFBundleVersion'] = 0;
+        // }
+        //
+        // // android-versionCode doen't exist in config.xml
+        // if(typeof obj['widget']['$']['android-versionCode'] === 'undefined') {
+        //     obj['widget']['$']['android-versionCode'] = 0;
+        // }
+        //
+        // // Increment build numbers (separately for iOS and Android)
+        // obj['widget']['$']['ios-CFBundleVersion']++;
+        // obj['widget']['$']['android-versionCode']++;
+
+
+        obj['widget']['$']['version'] = packageJson.version;
+        // console.log(obj);
+        // Build XML from JS Obj
+        var builder = new xml2js.Builder();
+        var xml = builder.buildObject(obj);
+
+        // Write config.xml
+        fs.writeFile('config.xml', xml, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+
+            //console.log('Build number successfully incremented');
+        });
+
+    });
+});
