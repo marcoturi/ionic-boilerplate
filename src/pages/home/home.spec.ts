fc420eb8b11ca879a24540ac6e0ff50d63d80466
Michel Felipe <mfelipeof@gmail.com>
2016-10-13 14:02:52 -0300

    refactor(unit-test): Change structure to add specs to run in runtime

--- src/pages/home/home.spec.ts
+++ src/pages/home/home.spec.ts
@@ -1,26 +1,17 @@
 import { HomePage } from './home';
-import { async, TestBed } from '@angular/core/testing';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { mockNavController } from 'ionic-angular/util/mock-providers';
-import { NavController } from 'ionic-angular';
+import { async } from '@angular/core/testing';
+import { ComponentTest } from '../../../config/component-test';
 
 describe('Sales Service', () => {
 
+    let helper = new ComponentTest<HomePage>();
     beforeEach((done) => {
-        TestBed.configureTestingModule({
-            declarations: [HomePage],
-            schemas: [CUSTOM_ELEMENTS_SCHEMA],
-            providers: [
-                {provide: NavController, useValue: mockNavController},
-            ],
-        });
-        TestBed.compileComponents();
+        helper.init(HomePage);
         done();
     });
 
     it('should load component', async(() => {
-        const fixture = TestBed.createComponent(HomePage);
-        fixture.detectChanges();
-        expect(fixture).toBeDefined();
+        helper.fixture.detectChanges();
+        expect(helper.fixture).toBeDefined();
     }));
 });
