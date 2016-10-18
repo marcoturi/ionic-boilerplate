2274110a7a75ab1fe0bef324b175ecc93f9d3e22
Marco Turi <marco.turi@hotmail.it>
2016-10-18 21:29:23 +0200

    feat(CI): add automatic download of .ipa

--- sh/ionic-package-ios.sh
+++ sh/ionic-package-ios.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-#inspired by https://github.com/devillex/wercker-ionic-package-step/blob/master/run.sh
+#inspired by
+# https://github.com/devillex/wercker-ionic-package-step/blob/master/run.sh
+# https://github.com/lewisl9029/toc/blob/50a96bc88035892083f87337dced9b9fe8c6d979/scripts/toc-package-download.sh
+
 cli="ionic"
 
 main() {
@@ -22,11 +25,41 @@ main() {
 
 	# profile
   if [ -n "$IONIC_PACKAGE_BUILD_PROFILE" ]; then
-    args="$args --profile \"$IONIC_PACKAGE_BUILD_PROFILE\""
+    args="$args --profile $IONIC_PACKAGE_BUILD_PROFILE"
   fi
 
+  finalCmd="$cli $cmd $args"
 
-  eval "$cli" "$cmd" "$args"
+  # echo $finalCmd;
+
+  PACKAGE_ID="$($finalCmd | \
+  grep "Build ID: " | \
+  sed -E 's/Build ID: ([0-9]+)/\1/')" #on linux use sad -r on osx sad -E
+
+  # echo $grepBuildId
+
+  if [ "$PACKAGE_ID" == "" ]
+    then
+      echo "No package id specified"
+      exit 2
+    fi
+
+    MAX_ATTEMPTS=15
+    current_attempt=1
+    echo "Downloading $1 package $PACKAGE_ID"
+    until ionic package download $PACKAGE_ID || \
+      [ "$current_attempt" == "$MAX_ATTEMPTS" ]
+    do
+      echo "Package download attempt $current_attempt failed. Trying again in 10 seconds."
+      current_attempt=$((current_attempt+1))
+      sleep 10
+    done
+
+  if [ "$current_attempt" == "$MAX_ATTEMPTS" ]
+    then
+      echo "Max attempts exceeded"
+      exit 2
+    fi
 }
 
 
