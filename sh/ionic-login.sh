1761cdf24a4d67091ac231befa76ab6871663b7c
Marco Turi <marco.turi@hotmail.it>
2016-10-18 17:35:45 +0200

    chore(gitlab): fix typo and clean ionic-login

--- sh/ionic-login.sh
+++ sh/ionic-login.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 # Inspired by https://github.com/devillex/wercker-ionic-login-step/blob/master/run.sh
 cli="ionic"
 
 main() {
-  display_info
 
   if [ -z "$IONIC_LOGIN_EMAIL" ]; then
     fail "ionic-login: email argument cannot be empty"
@@ -30,19 +29,7 @@ main() {
     args="$args --password \"$IONIC_LOGIN_PASSWORD\""
   fi
 
-	# Check if running in debug mode
-  info "Running ionic command"
-  if [ "$IONIC_LOGIN_DEBUG" = "true" ]; then
-    info "$cli $cmd $args"
-  fi
-
   eval "$cli" "$cmd" "$args"
-}
-
-display_info() {
-  info "Running ionic info:"
-  "$cli" info
-  echo ""
 }
 
 main;
