860289976a382efad019d59306a6dc92fdef85fd
Marco Turi <marco.turi@hotmail.it>
2016-10-22 19:44:08 +0200

    feat(src): add lodash-es example

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import filter from 'lodash-es/filter';
 
 @Component({
     selector: 'page-home',
@@ -9,25 +10,19 @@ export class HomePage {
     public user;
 
     constructor(public navCtrl: NavController) {
-        // console.log(process.env.NODE_ENV);
-        // const myArr = [
-        //     {
-        //         name: 'barney',
-        //         age: 36,
-        //         active: true,
-        //     },
-        //     {
-        //         name: 'fred',
-        //         age: 40,
-        //         active: false,
-        //     }];
-        //
-        // this.user = (_.filter(myArr, o => !o.active))[0];
-        this.user = {
-                    name: 'barney',
-                    age: 36,
-                    active: true,
-                };
+        const myArr = [
+            {
+                name: 'barney',
+                age: 36,
+                active: true,
+            },
+            {
+                name: 'fred',
+                age: 40,
+                active: false,
+            }];
+
+        this.user = (filter(myArr, o => o.active))[0];
     }
 
 }
