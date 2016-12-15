6fab1e1109dea360c226c24c6e544f5c3f789735
Marco <marco.turi@hotmail.it>
2016-12-15 11:02:30 +0100

    fix(tests): add angular2-template-loader to load templateUrls #17

--- config/webpack.test.js
+++ config/webpack.test.js
@@ -127,17 +127,21 @@ module.exports = (options) => {
                  */
                 {
                     test: /\.ts$/,
-                    loader: 'awesome-typescript-loader',
-                    query: {
-                        // use inline sourcemaps for "karma-remap-coverage" reporter
-                        sourceMap: !coverageEnabled,
-                        inlineSourceMap: coverageEnabled,
-                        module: 'commonjs',
-                        noEmitHelpers: true,
-                        compilerOptions: {
-                            removeComments: true
-                        }
-                    },
+
+                    loaders: [
+                        `awesome-typescript-loader?sourceMap=${!coverageEnabled},inlineSourceMap=${coverageEnabled},module=commonjs,noEmitHelpers=true,compilerOptions{}=removeComments:true`,
+                        'angular2-template-loader'
+                    ],
+                    // query: {
+                    //     // use inline sourcemaps for "karma-remap-coverage" reporter
+                    //     sourceMap: !coverageEnabled,
+                    //     inlineSourceMap: coverageEnabled,
+                    //     module: 'commonjs',
+                    //     noEmitHelpers: true,
+                    //     compilerOptions: {
+                    //         removeComments: true
+                    //     }
+                    // },
                     exclude: [/\.e2e\.ts$/]
                 },
 
